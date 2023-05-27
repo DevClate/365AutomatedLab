1033770aab1fef34e9f5caac8272ffb9e20b794d
DevClate <107187714+DevClate@users.noreply.github.com>
2023-05-27 08:24:23 -0400

    Removed variable for session

--- Functions/Public/Remove-CT365Group.ps1
+++ Functions/Public/Remove-CT365Group.ps1
@@ -38,7 +38,7 @@ function Remove-CT365Group {
     Import-Module ImportExcel
 
     # Connect to Exchange Online
-    $exoSession = Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName -ShowProgress $true
+    Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName -ShowProgress $true
     
     # Connect to Microsoft Graph
     Connect-MgGraph -Scopes "Group.ReadWrite.All"
