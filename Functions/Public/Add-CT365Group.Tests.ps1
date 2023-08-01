601252caa07cd0e550b1ddf93f5fa8a680b3b321
Christian Ritter <h.christian.ritter@gmail.com>
2023-08-01 16:34:36 +0200

    Improved Pester test coverage for Add-CT365Group and fixed issues with Add-CT365GroupByTitle Pester test.
    Refactored the Add-CT365Group function:
    
    Implemented parameter validation to ensure proper input values.
    Changed the creation of the groups based on their types to reduce code duplication and enhance maintainability.
    Removed the check for group existence as we can't create a group if it already exists.
    Reduced the number of Write-PSFMessage calls in the function to enhance code readability and maintainability.

--- Functions/Public/Add-CT365Group.Tests.ps1
+++ Functions/Public/Add-CT365Group.Tests.ps1
@@ -1,20 +1,20 @@
 BeforeAll {
-    . $PSScriptRoot/Add-CT365GroupByTitle.ps1
+    . $PSScriptRoot/Add-CT365Group.ps1
 }
 
-Describe 'Add-CT365GroupByTitle Function' {
+Describe 'Add-CT365Group Function' {
     Context 'When provided invalid parameters' {
         It 'Should throw an error for invalid domain format' {
             $filePath = "C:\Path\to\file.xlsx"
             $domain = "invalid_domain"
             
-            { Add-CT365GroupByTitle -FilePath $filePath -Domain $domain } | Should -Throw
+            { Add-CT365Group -FilePath $filePath -Domain $domain } | Should -Throw
         }
         It 'Should throw an error for invalid file path' {
             $filePath = "C:\Invalid\Path\file.xlsx"
             $domain = "contoso.com"
             
-            { Add-CT365GroupByTitle -FilePath $filePath -Domain $domain } | Should -Throw
+            { Add-CT365Group -FilePath $filePath -Domain $domain } | Should -Throw
         }
     }
 }
