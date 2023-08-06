f6c08386b6ac4c1ae8e6035c37f748938953846d
Clayton Tyger <gitclate@clatent.com>
2023-08-06 05:56:18 -0400

    Added comment based help

--- Functions/Public/New-CT365DataEnvironment.ps1
+++ Functions/Public/New-CT365DataEnvironment.ps1
@@ -1,6 +1,42 @@
-# Import the module
-Import-Module ImportExcel
+<#
+.SYNOPSIS
+Creates a new Excel workbook with predefined worksheets for Office 365 data.
 
+.DESCRIPTION
+The New-CT365DataEnvironment function creates a new Excel workbook with predefined worksheets. 
+The workbook contains a "Users" worksheet and a "Groups" worksheet, and additional worksheets based on the provided JobRole parameter.
+
+.PARAMETER WorkbookName
+The name of the workbook file to be created. It must be a .xls or .xlsx file.
+
+.PARAMETER FilePath
+The path where the workbook will be created. The provided path must exist.
+
+.PARAMETER JobRole
+An array of strings specifying additional worksheets to be created in the workbook. 
+Each string will be used as the name of a new worksheet.
+
+.EXAMPLE
+New-CT365DataEnvironment -WorkbookName "myworkbook.xlsx" -FilePath "C:\temp" -JobRole "Manager","Employee"
+
+This command creates an Excel workbook named "myworkbook.xlsx" in the "C:\temp" directory.
+The workbook will contain the "Users" and "Groups" worksheets, as well as a "Manager" and "Employee" worksheet.
+
+.EXAMPLE
+New-CT365DataEnvironment -WorkbookName "365DataEnvironment.xlsx" -FilePath "C:\365DevEnvironment" -JobRole "NY-ITManager","CA-AccountsPayable","FL-HumanResources"
+
+This command creates an Excel workbook named "365DataEnvironment.xlsx" in the "C:\365DevEnvironment" directory.
+The workbook will contain the "Users" and "Groups" worksheets, as well as a "NY-ITManager", "CA-AccountsPayable", and "FL-HumanResources" worksheet.
+
+.INPUTS
+System.String
+
+.OUTPUTS
+None. This cmdlet does not return any output.
+
+.NOTES
+The JobRole Parameter can be a single job role or a location and job role. If you are a company with only one location, you do not need to put a location in.
+#>
 function New-CT365DataEnvironment {
     [CmdletBinding()]
     param (
