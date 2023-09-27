340d75b80533968920be0b86ca6354427ec1f4fc
Clayton Tyger <gitclate@clatent.com>
2023-09-27 05:31:48 -0400

    Changed add to New

--- Tests/New-CT365User.Tests.ps1
+++ Tests/New-CT365User.Tests.ps1
@@ -1,20 +1,20 @@
 BeforeAll {
-    . $PSScriptRoot/Add-CT365User.ps1
+    . $PSScriptRoot/New-CT365User.ps1
 }
 
-Describe 'Add-CT365User Function' {
+Describe 'New-CT365User Function' {
     Context 'When provided invalid parameters' {
         It 'Should throw an error for invalid domain format' {
             $filePath = "C:\Path\to\file.xlsx"
             $domain = "invalid_domain"
             
-            { Add-CT365User -FilePath $filePath -Domain $domain } | Should -Throw
+            { New-CT365User -FilePath $filePath -Domain $domain } | Should -Throw
         }
         It 'Should throw an error for invalid file path' {
             $filePath = "C:\Invalid\Path\file.xlsx"
             $domain = "contoso.com"
             
-            { Add-CT365User -FilePath $filePath -Domain $domain } | Should -Throw
+            { New-CT365User -FilePath $filePath -Domain $domain } | Should -Throw
         }
     }
 }
