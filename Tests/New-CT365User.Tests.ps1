122f38302a1b5686a159030c53627853724294b0
Clayton Tyger <gitclate@clatent.com>
2023-10-15 06:31:36 -0400

    Updated testing

--- Tests/New-CT365User.Tests.ps1
+++ Tests/New-CT365User.Tests.ps1
@@ -1,11 +1,14 @@
 BeforeAll {
-    . $PSScriptRoot/New-CT365User.ps1
+    # Call Cmdlet
+    $commandScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '..\functions\public\New-CT365User.ps1'
+
+    . $commandScriptPath
 }
 
 Describe 'New-CT365User Function' {
     Context 'When provided invalid parameters' {
         It 'Should throw an error for invalid domain format' {
-            $filePath = "C:\Path\to\file.xlsx"
+            $filePath = $commandScriptPath
             $domain = "invalid_domain"
             
             { New-CT365User -FilePath $filePath -Domain $domain } | Should -Throw
