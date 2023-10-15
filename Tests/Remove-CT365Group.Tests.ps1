364be34d7033a25b3c3df7682fc96542c28dd2a3
Clayton Tyger <gitclate@clatent.com>
2023-10-15 06:47:27 -0400

    Updated Pester Tests

--- Tests/Remove-CT365Group.Tests.ps1
+++ Tests/Remove-CT365Group.Tests.ps1
@@ -1,12 +1,21 @@
 BeforeAll {
-    . $PSScriptRoot/Remove-CT365Group.ps1
+    # Call Cmdlet
+    $commandScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '..\functions\public\Remove-CT365Group.ps1'
+
+    . $commandScriptPath
 }
 
 Describe 'Remove-CT365Group Function' {
     Context 'When provided invalid parameters' {
         It 'Should throw an error for invalid domain format' {
-            $filePath = "C:\Path\to\file.xlsx"
+            $filePath = $commandScriptPath
             $domain = "invalid_domain"
+            
+            { Remove-CT365Group -FilePath $filePath -Domain $domain } | Should -Throw
+        }
+        It 'Should throw an error for invalid file path' {
+            $filePath = "C:\Invalid\Path\file.xlsx"
+            $domain = "contoso.com"
             
             { Remove-CT365Group -FilePath $filePath -Domain $domain } | Should -Throw
         }
